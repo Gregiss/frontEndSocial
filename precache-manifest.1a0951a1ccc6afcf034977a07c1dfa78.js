5868b8f81ffa019b54ac00e3caea1ad67436a565
Hurricane404 <46573030+Hurricane404@users.noreply.github.com>
2020-09-07 17:22:41 -0300

    Add files via upload

---
+++ precache-manifest.1a0951a1ccc6afcf034977a07c1dfa78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4e10d313942acfa2fc663c461641b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddb15fade2bee948605f",
+    "url": "/static/css/2.9689ac4b.chunk.css"
+  },
+  {
+    "revision": "1f5a504b21406fc36dc3",
+    "url": "/static/css/main.de9c673c.chunk.css"
+  },
+  {
+    "revision": "ddb15fade2bee948605f",
+    "url": "/static/js/2.dd94e2b6.chunk.js"
+  },
+  {
+    "revision": "216de28b4d188779ecca3f991ca94cbc",
+    "url": "/static/js/2.dd94e2b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5a504b21406fc36dc3",
+    "url": "/static/js/main.3f725b91.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
